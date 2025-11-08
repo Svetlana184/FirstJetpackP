552f78df5175f9fa5eab4ef4f1c1d766920771b8
Svetlana184 <sveta.kgd.39@gmail.com>
2025-11-08 13:22:11 +0200

    temp

--- app/src/main/java/com/example/firstjetpack/MainActivity.kt
+++ app/src/main/java/com/example/firstjetpack/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,11 +19,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,33 +42,53 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FirstJetpackTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+//                FirstJetpackTheme {
+//                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                        Hello()
+//                    }
+//                }
+            NewStory()
         }
     }
 }
 
 @Composable
-fun Hello(){
-    Text(
-        text = "tytyyyyyyyyyytytytttttttttttttt" +
-                "tttttttttttttttttt" +
-                "ytytytytt" +
-                "tttttt" +
-                " ytyty ytytyyyyyytytytyty " +
-                "yttttttttttttttttttytyty" +
-                " ytytytytytytyty",
-
-        style = TextStyle(fontSize = 20.sp),
-        modifier = Modifier.horizontalScroll(ScrollState(0))
-    )
+fun NewStory(){
+    MaterialTheme() {
+        val typography = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(top=50.dp, start=16.dp, end = 16.dp, bottom = 16.dp)
+        ) {
+            Image(bitmap = ImageBitmap.imageResource(R.drawable.pic),
+                modifier = Modifier.height(180.dp).fillMaxWidth().
+                clip(shape = RoundedCornerShape(15.dp)),
+                contentDescription = "world",
+                contentScale = ContentScale.Crop
+            )
+            Spacer(Modifier.height(16.dp))
+            Text("Day in Shark Cove", style = typography.bodyLarge)
+            Text("California", style = typography.bodySmall)
+            Text("December 2018")
+        }
+    }
 }
+
+
+//@Composable
+//fun Hello(){
+//    Text(
+//        text = "tytyyyyyyyyyytytytttttttttttttt" +
+//                "tttttttttttttttttt" +
+//                "ytytytytt" +
+//                "tttttt" +
+//                " ytyty ytytyyyyyytytytyty " +
+//                "yttttttttttttttttttytyty" +
+//                " ytytytytytytyty",
+//
+//        style = TextStyle(fontSize = 20.sp),
+//        modifier = Modifier.horizontalScroll(ScrollState(0))
+//    )
+//}
 //fun getTime(): String{
 //    val calendar = Calendar.getInstance()
 //    val hours = calendar.get(Calendar.HOUR_OF_DAY)
@@ -70,37 +99,37 @@ fun Hello(){
 //    return "nice day"
 //}
 
-@Composable
-fun MyList(){
-    val langs = listOf("c#", "c++", "java", "go")
-    Column(){
-        for (lang in langs){
-            Text(text = lang, fontSize = 30.sp,
-                modifier = Modifier.
-                padding(start = 0.dp, top = 10.dp, end = 20.dp, bottom = 10.dp).
-                size(width = 100.dp, height = 200.dp).
-                offset(x = 30.dp, y = 50.dp).
-                background(Color.LightGray)
-
-            )
-
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//@Composable
+//fun MyList(){
+//    val langs = listOf("c#", "c++", "java", "go")
+//    Column(){
+//        for (lang in langs){
+//            Text(text = lang, fontSize = 30.sp,
+//                modifier = Modifier.
+//                padding(start = 0.dp, top = 10.dp, end = 20.dp, bottom = 10.dp).
+//                size(width = 100.dp, height = 200.dp).
+//                offset(x = 30.dp, y = 50.dp).
+//                background(Color.LightGray)
+//
+//            )
+//
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
 
 @Preview(showBackground = true)
 
 @Composable
-fun GreetingPreview() {
-        MyList()
+fun HelloPreview() {
+    NewStory()
 }
 
 
